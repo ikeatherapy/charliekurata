a8d651520ce44c352a94d340d3b0056163a5700c
noe <90001579+nkurata@users.noreply.github.com>
2024-04-26 16:10:05 +0200

    feat: add javascript file for resize + check mobile status

---
+++ main.js
@@ -0,0 +1,22 @@
+window.addEventListener('resize', function() {
+    var width = window.innerWidth;
+    if (width <= 768 || /Mobi|Android/i.test(navigator.userAgent)) {
+        document.body.style.alignItems = 'center';
+        document.body.style.justifyContent = 'center';
+        var btnGroup = document.querySelector('.btn-group');
+        btnGroup.style.justifyContent = 'center';
+        btnGroup.style.alignItems = 'center';
+        var imageContainer = document.querySelector('.image-container');
+        imageContainer.style.display = 'none';
+    } else {
+        document.body.style.alignItems = '';
+        document.body.style.justifyContent = '';
+        var btnGroup = document.querySelector('.btn-group');
+        btnGroup.style.justifyContent = '';
+        btnGroup.style.alignItems = '';
+        var imageContainer = document.querySelector('.image-container');
+        imageContainer.style.display = '';
+    }
+});
+
+window.dispatchEvent(new Event('resize'));
