a33d03a786d94360676052519555dc33410c472f
noe <90001579+nkurata@users.noreply.github.com>
2024-05-17 11:23:51 +0200

    fix: buttons size change if resize

--- main.js
+++ main.js
@@ -3,13 +3,19 @@ window.addEventListener('resize', function() {
     var btnGroup = document.querySelector('.btn-group');
     var imageContainer = document.querySelector('.image-container');
     var logo = document.querySelector('.image-container-top-right');
-    if (width <= 768 || /Mobi|Android/i.test(navigator.userAgent)) {
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
         document.body.style.justifyContent = 'center';
         btnGroup.style.marginLeft = '0';
         btnGroup.style.transform = 'scale(1.8)';
         logo.style.right = '40px';
         logo.style.top = '40px';
         logo.style.transform = 'scale(1.5)';
+        imageContainer.style.display = 'none';
+    } else if (width <= 768) {
+        document.body.style.alignItems = 'center';
+        document.body.style.justifyContent = 'center';
+        btnGroup.style.justifyContent = 'center';
+        btnGroup.style.alignItems = 'center';
         imageContainer.style.display = 'none';
     } else {
         document.body.style.alignItems = '';
